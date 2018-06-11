a890fdbe43ee4bd18d371eb31465475b934d42af
skageraz <39991372+skageraz@users.noreply.github.com>
2018-06-11 19:54:58 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+setTimeout(function () { alert('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please'); }, 10000);
+
+function color(){
+
+    document.body.style.backgroundColor = "magenta";
+    document.body.style.color = "blue";
+    document.getElementById("header").style.color = "green";
+
+   }
